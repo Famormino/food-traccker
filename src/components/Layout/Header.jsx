91a9663cfc5d285669b6669b78d3f5e45e03fd05
Federico Amormino <Federico.Amormino@gmail.com>
2023-04-22 16:32:51 -0300

    adding Meals section

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,18 +1,18 @@
-import React, { Fragment } from "react";
+import { Fragment } from "react";
 import classes from "./Header.module.css";
 
 import mealsImage from "../../assets/meals.jpg";
 import HeaderCartButton from "./HeaderCartButton";
 
-const Header = (props) => {
+const Header = () => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>React Meals</h1>
         <HeaderCartButton />
       </header>
-      <div className={classes['main-image']}>
-        <img src={mealsImage} alt="A table full of delicious food!"/>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="A table full of delicious food!" />
       </div>
     </Fragment>
   );
