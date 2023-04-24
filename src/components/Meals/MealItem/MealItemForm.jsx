2739597237bf7434ac68aef0d28f0cacb748ba00
Federico Amormino <Federico.Amormino@gmail.com>
2023-04-24 09:21:47 -0300

    adding Input componente to UI

--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,10 +1,22 @@
+import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = () => {
-  return <form className={classes.form}>
-      <input />
+  return (
+    <form className={classes.form}>
+      <Input
+        label="Amount"
+        input={{
+          type: "number",
+          min: 1,
+          max: 5,
+          step: 1,
+          defaultValue: 1,
+        }}
+      />
       <button>+ Add</button>
-  </form>;
+    </form>
+  );
 };
 
 export default MealItemForm;
